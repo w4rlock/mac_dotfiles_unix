a0b719fcc3818e8f86f12f996f3671cdcd2208e8
Emanuel Rosal <emanuel.rosal@externos-cl.cencosud.com>
2021-06-06 19:32:21 -0300

    new tools

--- .vim/templates/skeleton.sh
+++ .vim/templates/skeleton.sh
@@ -5,18 +5,28 @@ set -o pipefail
 set -o nounset
 set -o errexit
 
-# colors
-T_BOLD=$(tput bold)
-C_B_RED="${T_BOLD}$(tput setaf 1)"
-C_B_BLUE="${T_BOLD}$(tput bold)$(tput setaf 4)"
+# colors utils
+BOLD=$(tput bold)
 C_RESET=$(tput sgr0)
 
+
+red() {   printf "${BOLD}$(tput setaf 1)${1}${C_RESET}" ; }
+blue() {  printf "${BOLD}$(tput setaf 4)${1}${C_RESET}" ; }
+green() { printf "${BOLD}$(tput setaf 2)${1}${C_RESET}" ; }
+log_info() { echo "$(blue '[')${BOLD}*$(blue ']') - ${@}" ; }
+
+
+
+# Show msg and exit program
 die() {
-  echo "${C_B_RED}Error:${C_RESET} ${@}" >&2
+  echo "$(red 'Error:') ${@}" >&2
   exit 1
 }
 
 
+
+# Util to check if your script has dependencies example:
+# check_required_programs wget rg fd fzf curl
 check_required_programs() {
   for dep in ${@}; do
     which $dep &> /dev/null || die "program '${dep}' is required."
@@ -24,22 +34,48 @@ check_required_programs() {
 }
 
 
-log_info() {
-  echo "${C_B_BLUE}[${C_RESET} ${T_BOLD}*${C_RESET} ${C_B_BLUE}]${C_RESET} - ${@}"
-}
-
 
 usage() {
   cat <<EOF
 
-${C_B_BLUE}Usage:${C_RESET} $0 [options] [args]
+$(green 'Script Utility for ...')
 
-${C_B_BLUE}Options:${C_RESET}
-  -h          This help screen"
+$(blue 'Usage:')
+    $(green $0) [options] [args]
 
+$(blue 'Options:')
+    -e | --example        (required)      Description here
+    -h | --help                           This help screen"
 EOF
   exit 0
 }
 
-#check_required_programs wget curl
+
+parse_arguments() {
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -e|--example)
+      EXAMPLE_VALUE="$2"
+      shift # past value
+      ;;
+      --default)
+      DEFAULT=YES
+      ;;
+      -h|--help)
+      usage
+      ;;
+      *)
+      echo $(red "ERR: unknown option ${1}") >&2
+      usage
+      ;;
+    esac
+
+    shift  # past key
+  done
+}
+
+parse_arguments $@
+
+
+
 
